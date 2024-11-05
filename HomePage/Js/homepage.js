51b8c67ce35fea7b4b5cafc3cd6f1be649eb6717
Henrique Ribeiro <henrique@proansi.com.br>
2024-11-04 21:34:02 -0300

    Ajuste HomePage

--- HomePage/Js/homepage.js
+++ HomePage/Js/homepage.js
@@ -1,19 +1,18 @@
+// Botão "Entrar na Plataforma" com redirecionamento
 document.getElementById('entrarBtn').addEventListener('click', function(event) {
-    event.preventDefault(); // Impede o comportamento padrão do link
-    window.location.href = '../selecao.html'; // Redireciona para a nova página
+    event.preventDefault();
+    window.location.href = '../selecao.html';
 });
-// Seleciona todos os links do menu de navegação
+
+// Navegação suave para seções
 document.querySelectorAll(".nav-link").forEach(link => {
     link.addEventListener("click", function(event) {
-        event.preventDefault(); // Previne o comportamento padrão do link
-
-        // Seleciona a seção para a qual deseja rolar
+        event.preventDefault();
         const targetId = this.getAttribute("href").substring(1);
         const targetSection = document.getElementById(targetId);
 
-        // Faz a rolagem suave até a seção alvo
         targetSection.scrollIntoView({
-            behavior: "smooth" // Rolagem suave
+            behavior: "smooth"
         });
     });
 });
