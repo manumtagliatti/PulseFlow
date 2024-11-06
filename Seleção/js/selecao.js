434360f94b53836f858001a00c094cc1b9aa49ae
Henrique Ribeiro <henrique@proansi.com.br>
2024-11-06 01:36:37 -0300

    Ajuste Seleçao

--- Seleção/js/selecao.js
+++ Seleção/js/selecao.js
@@ -4,10 +4,10 @@ function submitRole() {
 
     if (selectedRole === 'medico') {
         // Redireciona para a página loginMedico dentro da pasta Médico
-        window.location.href = 'Médico/loginMedico.html';
+        window.location.href = '../Médico/loginMedico.html';
     } else if (selectedRole === 'paciente') {
         // Redireciona para a página loginPaciente dentro da pasta Paciente
-        window.location.href = 'Paciente/loginPaciente.html';
+        window.location.href = '../Paciente/loginPaciente.html';
     } else {
         alert('Por favor, selecione uma opção antes de continuar.'); // Alerta caso nenhuma opção seja selecionada
     }
